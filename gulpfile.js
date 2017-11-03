8fcb9bf10a5268df3973370c882abd32f0fa7237
nikulio <sn97@icloud.com>
2017-11-03 10:41:05 +0100

    hello world

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function () {
+	return gulp.src('src/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('src/css'))
+});
+
+gulp.task('watch', function () {
+	gulp.watch('src/scss/*.scss', ['sass']);
+})
