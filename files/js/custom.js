51d8725afbd2ec15a225167511f1bc39368e77e7
nikulio <sn97@icloud.com>
2017-11-07 22:23:27 +0100

    adjustments for wizard validation, buggy

--- files/js/custom.js
+++ files/js/custom.js
@@ -51,9 +51,7 @@ function bindingWizardsTabs() {
 
 //added skip button for wizard control
 function appendSkipWizardButton() {
-	$('.sf-controls').append('' +
-		'<a class="skip-btn sf-right sf-btn sf-btn-next" onclick="skipStep()" href="#">SKIP</a>'
-	);
+	$('.sf-controls').append('' + '<a class="skip-btn sf-right sf-btn sf-btn-next" onclick="" href="#">SKIP</a>');
 }
 
 // Global variables leave here, pls
@@ -88,35 +86,12 @@ function populateExploreBlock() {
 			var tokens = result[0].explore
 			tokens.map(function (data) {
 
-				block.append('' +
-					'<div class="isotope-item blog-item" style="position: absolute; left: 0px; top: 0px;">' +
-
+				block.append('' + '<div class="isotope-item blog-item" style="position: absolute; left: 0px; top: 0px;">' +
 					//TODO: if we want to return hover, then should add thumb-overlay overlay-effect-1 to text-light
-					'<div class="blog-media">' +
-					'					<a href="portfolio-single-custom.html" class="text-light"> ' +
-					'<img src=' + data.img + ' alt="SEO IMG NAME">' +
-					'<div class="overlay-caption hidden-on-start">' +
-					'<h6 class="caption-sub portfolio-category subtitle-2"></h6>' +
-					'<h4 class="caption-name portfolio-name uppercase"></h4>' +
-					'</div>' +
-					'</a>' +
-					'</div>' +
-
-					'<div class="blog-desc align-center">' +
-					'<div class="blog-headline">' +
-					'<h6 class="post-category uppercase">' + data.category + '</h6>' +
-					'<h5 class="post-name"><a href="blog-single-custom1.html"><strong>' + data.title + '</strong></a></h5>' +
-					'</div>' +
-					'<p>' +
-					data.intro +
-					'</p>' +
-					'</div>' +
-					'</div>'
-				);
+					'<div class="blog-media">' + '					<a href="portfolio-single-custom.html" class="text-light"> ' + '<img src=' + data.img + ' alt="SEO IMG NAME">' + '<div class="overlay-caption hidden-on-start">' + '<h6 class="caption-sub portfolio-category subtitle-2"></h6>' + '<h4 class="caption-name portfolio-name uppercase"></h4>' + '</div>' + '</a>' + '</div>' + '<div class="blog-desc align-center">' + '<div class="blog-headline">' + '<h6 class="post-category uppercase">' + data.category + '</h6>' + '<h5 class="post-name"><a href="blog-single-custom1.html"><strong>' + data.title + '</strong></a></h5>' + '</div>' + '<p>' + data.intro + '</p>' + '</div>' + '</div>');
 			})
 		}
 	});
-
 }
 
 //Show/hide header
@@ -132,8 +107,7 @@ function manipulatingHeader() {
 
 		if (scrollFromTop > topBlockHeight) {
 			header.addClass('custom-visible')
-		}
-		else {
+		} else {
 			header.removeClass('custom-visible')
 		}
 	})
@@ -156,93 +130,108 @@ function loadTokens() {
 			currentTokens.html(sold.formatMoney(0, '.', ','));
 			allTokens.html(total.formatMoney(0, '.', ','));
 		}
-	}) 
+	})
 }
-
 
 //init of wizard steps
 function wizardInit() {
+	var server = "https://private-anon-a04cf432bf-legalwaves.apiary-mock.com/api";
+
+
+	function errorOnInput(data) {
+		sfw.activeNext(false); // disable next button
+		// sfw.activeStep(1, false); // deactivate next step
+
+		if ($('#text').val() != "") {
+			if (next_loading) { // test if ajax is executing
+				return false;
+			}
+			if (data !== undefined && data.done) { // if ajax is done, than data.done is set to true
+				return true;
+			}
+
+			sfw.addSpinner('next'); // add spinner to next button
+			next_loading = true; // to prevent stack of ajax requests if user hasn't patience
+			$.ajax({
+				url: server,
+				success: function () {
+					$('#text-error.error').removeClass('show');
+					$('#text').removeClass('error-input');
+					sfw.addSpinner('next', false); // remove spinner
+					next_loading = false; // allow next step
+
+					sfw.activeNext(true); // disable next button
+					sfw.next(false, {done: true}) // go to next step, additional data will be in next callback
+					$('#text').val('')
+					$('#wizard-box.sf-t2 .sf-btn.skip-btn').removeClass('visible');
+
+				}
+			});
+			return false;
+		}
+		else {
+			sfw.activeNext(false); // disable next button
+			// sfw.activeStep(1, false);
+			$('#text-error.error').addClass('show');
+			$('#text').addClass('error-input');
+			return false
+		}
+	}
 
 	sfw = $("#wizard").stepFormWizard({
 		onPrev: function (from) {
-			console.log(from);
 			if (from === 3) {
 				$('#wizard-box.sf-t2 .sf-btn.skip-btn').addClass('visible');
-			}
-			else {
+			} else {
 				$('#wizard-box.sf-t2 .sf-btn.skip-btn').removeClass('visible');
 			}
 		},
-		onNext: function (from, data) {
-			$('#text').val('');
+		onSlideChanged: function (step, data) {
+			if (step === 1) {
+				$(document).on('click', function (e) {
 
-			if (from === 1) {
+					if (!($(e.target).hasClass('skip-btn') ||
+							$(e.target).hasClass('sf-nav-step') ||
+							$(e.target).hasClass('prev-btn'))) {
+
+						sfw.activeNext(false); // disable next button
+						// sfw.activeStep(1, false); // deactivate next step
+					}
+				})
+			}
+			if (step === 2) {
+
+				sfw.activeNext(false); // disable next button
+				// sfw.activeStep(1, false); // deactivate next step
+
 				$('#wizard-box.sf-t2 .sf-btn.skip-btn').addClass('visible');
-			}
-			if (from === 2) {
-				$('#wizard-box.sf-t2 .sf-btn.skip-btn').removeClass('visible');
-			}
+				$(document).on('click', function (e) {
 
-			if ($('#text').val() != "") {
-				if (next_loading) { // test if ajax is executing
-					return false;
-				}
-				if (data !== undefined && data.done) { // if ajax is done, than data.done is set to true
-					return true;
-				}
-				sfw.addSpinner('next'); // add spinner to next button
-				next_loading = true; // to prevent stack of ajax requests if user hasn't patience
+					if (!($(e.target).hasClass('skip-btn') ||
+							$(e.target).hasClass('sf-nav-step') ||
+							$(e.target).hasClass('prev-btn'))) {
+						errorOnInput(data);
 
-				var settings = {
-					"async": true,
-					"crossDomain": true,
-					"url": "http://mockbin.com/request?foo=bar&foo=baz",
-					"method": "POST",
-					"headers": {
-						"cookie": "foo=bar; bar=baz",
-						"accept": "application/json",
-						"content-type": "application/json",
-						"x-pretty-print": "2"
-					},
-					"processData": false,
-					"data": data
-				};
+					}
+					else {
+						next_loading = false; // allow next step
+						sfw.next(false, {done: true}) // go to next step, additional data will be in next callback
+						sfw.activeNext(true); // disable next button
+						// sfw.activeStep(2, true); // deactivate next step
+						$('#wizard-box.sf-t2 .sf-btn.skip-btn').removeClass('visible');
+						return false
+					}
+				})
+			} 
+		},
 
-				$.ajax(settings).done(function (response) {
-					console.log(response);
-				});
-
-				return false;
-			}
-		}
 	});
-
-	// sfw.activeNext(false); // disable next button
-
-	// $("#wizard").on('keyup', '#text', function () {
-	// 	if ($(this).val() != "") {
-	// 		sfw.activeNext(true);
-	// 	} else {
-	// 		$('#wizard-box.sf-t2 .sf-btn.skip-btn').on('click', function () {
-	//
-	// 		})
-	// 		// sfw.activeNext(false); // disable next button
-	// 		// sfw.activeStep(1, false); // deactive next step
-	// 	}
-	// })
-
 
 	$(".js-open-wizard").on('click', function (e) {
 		e.preventDefault();
 		$('html').addClass('lock')
 		$('.popup-wizard').removeClass('popup-hide')
 	})
-}
-
-function skipStep() {
-	next_loading = false; // allow next step
-	sfw.next(false, {done: true}) // go to next step, additional data will be in next callback
-	return false
 }
 
 
@@ -255,13 +244,29 @@ function closePopup() {
 	})
 }
 
-Number.prototype.formatMoney = function(c, d, t){
-  var n = this,
-    c = isNaN(c = Math.abs(c)) ? 2 : c,
-    d = d == undefined ? "." : d,
-    t = t == undefined ? "," : t,
-    s = n < 0 ? "-" : "",
-    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
-    j = (j = i.length) > 3 ? j % 3 : 0;
-  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+Number.prototype.formatMoney = function (c, d, t) {
+	var n = this,
+		c = isNaN(c = Math.abs(c))
+			? 2
+			: c,
+		d = d == undefined
+			? "."
+			: d,
+		t = t == undefined
+			? ","
+			: t,
+		s = n < 0
+			? "-"
+			: "",
+		i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+		j = (j = i.length) > 3
+			? j % 3
+			: 0;
+	return s + (
+		j
+			? i.substr(0, j) + t
+			: "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (
+		c
+			? d + Math.abs(n - i).toFixed(c).slice(2)
+			: "");
 };
