b6c50802275c6840b36d096425e9abcebe689eeb
sven4ask <sven@legalthings.io>
2017-11-07 14:28:44 +0100

    Format the tokens as money

--- files/js/custom.js
+++ files/js/custom.js
@@ -153,8 +153,8 @@ function loadTokens() {
 
 			$('.progress-active').attr('data-perc', (sold / total) * 100);
 
-			currentTokens.html(sold);
-			allTokens.html(total);
+			currentTokens.html(sold.formatMoney(0, '.', ','));
+			allTokens.html(total.formatMoney(0, '.', ','));
 		}
 	})
 }
@@ -255,6 +255,16 @@ function closePopup() {
 	})
 }
 
+Number.prototype.formatMoney = function(c, d, t){
+  var n = this,
+    c = isNaN(c = Math.abs(c)) ? 2 : c,
+    d = d == undefined ? "." : d,
+    t = t == undefined ? "," : t,
+    s = n < 0 ? "-" : "",
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+    j = (j = i.length) > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
 
 
 
