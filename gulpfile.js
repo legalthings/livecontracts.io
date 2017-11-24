8399448978f1d3a780e2029500f8d0c4f5404875
nikulio <sn97@icloud.com>
2017-11-24 14:09:49 +0100

    implemented optimization for files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,44 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('minif', function() {
+gulp.task('minify-css', function () {
+	return gulp.src('files/dist/*.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('files/dist'));
+});
+
+gulp.task('minif', function () {
 	gulp.src('./files/dist/*.js')
 		.pipe(minify({
-			ext:{
-				src:'.min.js',
-				min:'.js'
+			ext: {
+				src: '.min.js',
+				min: '.js'
 			},
 			exclude: ['tasks'],
 			ignoreFiles: ['.combo.js', '-min.js']
 		}))
 		.pipe(gulp.dest('./files/dist'))
+});
+
+gulp.task('compress-css', function () {
+	return gulp.src([
+		'files/css/style.css',
+		'files/css/font-awesome.min.css',
+		'files/css/ionicons.css',
+		// 'files/revolution/css/settings.css',
+		// 'files/revolution/css/layers.css',
+		// 'files/revolution/css/navigation.css',
+		'files/css/owl.carousel.css',
+		'files/css/lightcase.css',
+		'files/css/mqueries.css',
+		'files/css/step-form-wizard-all.css',
+		'files/css/jquery.mCustomScrollbar.min.css',
+	])
+		.pipe(concatCss("compress.css"))
+		.pipe(gulp.dest('files/dist/'));
 });
 
 gulp.task('compress', function () {
@@ -21,31 +47,31 @@ gulp.task('compress', function () {
 			'files/js/jquery-2.1.4.min.js',
 			'files/js/jquery.easing.1.3.js',
 			'files/js/jquery.visible.min.js',
-			'files/revolution/js/jquery.themepunch.tools.min.js?rev=5.0',
+			// 'files/revolution/js/jquery.themepunch.tools.min.js?rev=5.0',
 			
-			'files/revolution/js/jquery.themepunch.revolution.min.js?rev=5.0',
-			'files/revolution/js/extensions/revolution.extension.slideanims.min.js',
-			'files/revolution/js/extensions/revolution.extension.layeranimation.min.js',
-			'files/revolution/js/extensions/revolution.extension.navigation.min.js',
+			// 'files/revolution/js/jquery.themepunch.revolution.min.js?rev=5.0',
+			// 'files/revolution/js/extensions/revolution.extension.slideanims.min.js',
+			// 'files/revolution/js/extensions/revolution.extension.layeranimation.min.js',
+			// 'files/revolution/js/extensions/revolution.extension.navigation.min.js',
 			//
 			// 'files/revolution/js/extensions/revolution.extension.video.min.js',
 			// 'files/revolution/js/extensions/revolution.extension.actions.min.js',
 			// 'files/js/tweenMax.js',
-			// 'files/js/jquery.backgroundparallax.min.js',
+			'files/js/jquery.backgroundparallax.min.js',
 			//
-			// 'files/js/jquery.isotope.min.js',
-			// 'files/js/jquery.imagesloaded.min.js',
+			'files/js/jquery.isotope.min.js',
+			'files/js/jquery.imagesloaded.min.js',
 			// 'files/js/jquery.owl.carousel.js',
 			// 'files/js/jquery.lightcase.min.js',
 			//
-			// 'files/js/script.js',
-			// 'files/js/jquery.ajaxchimp.min.js',
-			// 'files/js/jquery.countdown.min.js',
-			// 'files/js/social.js',
+			'files/js/script.js',
+			'files/js/jquery.ajaxchimp.min.js',
+			'files/js/jquery.countdown.min.js',
+			'files/js/social.js',
 			//
-			// 'files/js/step-form-wizard.min.js',
-			// 'files/js/jquery.mCustomScrollbar.concat.min.js',
-			// 'files/js/custom.js'
+			'files/js/step-form-wizard.min.js',
+			'files/js/jquery.mCustomScrollbar.concat.min.js',
+			'files/js/custom.js'
 		])
 		.pipe(concat('compress.js'))
 		.pipe(gulp.dest('./files/dist'));
