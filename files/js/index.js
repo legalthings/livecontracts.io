a8456b5722e780a380550b76c46952b84701a66b
Benjamin <benjamin@legalthings.io>
2018-10-23 12:25:43 +0200

    Minor additions
    - New look Medium section
    - Added OSRE to clients

--- files/js/index.js
+++ files/js/index.js
@@ -14,14 +14,14 @@ $(function () {
 					 visibleSm = ' visible-sm';
 				 }
 				output += '<a href="'+ item.link +'" target="_blank"><div class="col-sm-6 col-md-4' + visibleSm + '">';
-				output += '<div class="medium-post"><header>';
+				output += '<div class="medium-post"><header class="align-center">';
 				/*output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";*/
 				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
 				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
 				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
-				output += '<div class="medium-element"><img class="img-responsive" src="' + src + '" width="340px" height="180px"></div></header>';
+				output += '<div class="medium-element"><img class="img-responsive medium-img" src="' + src + '" width="340px" height="180px"></div></header>';
 				output += '<div class="medium-content"><h4><a href="'+ item.link + ' " target="_blank">' + item.title + '</a></h4>';
 				output += '<span><hr></span>';
 				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
@@ -31,7 +31,7 @@ $(function () {
 				//re-trim if we are in the middle of a word
 				trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
 				output += '<div class="medium-text"><p>' + trimmedString + '..</p></div>';
-				output += '<div class="read-more"><a href="'+ item.link + ' target="_blank" class="read-more"> Read the full article </a></div>';
+				output += '<div class="read-more"><a href="'+ item.link + ' target="_blank" class="read-more"> Read the full article <i class="fa fa-arrow-right"></i></a></div>';
 				output += '</div></div></div></a>';
 				return k < 2;
 			});
